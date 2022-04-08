57a7c0985c985a50976cf4385d1e290bb318232f
joaquinrapetti <j.rapetti92@gmail.com>
2022-04-08 12:25:51 -0300

    motions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { GiKnifeFork } from 'react-icons/gi'
+import styled from "styled-components";
 import Category from "./components/Category";
 import Search from "./components/Search";
 import Pages from "./pages/Pages";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={'/'}>deliciousss</Logo>
+      </Nav>
         <Search />
         <Category />
         <Pages />
@@ -15,5 +22,22 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster Two', cursive;
+`
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`
 
 export default App;
